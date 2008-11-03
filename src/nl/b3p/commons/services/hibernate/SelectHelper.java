2a9b761e61924b6d8e0030cf858e319ce080d017
Chris van Lith <chrisvanlith@gmail.com>
2008-11-03 18:02:09 +0000

    copy op basis van reflectie verbeterd

--- src/nl/b3p/commons/services/hibernate/SelectHelper.java
+++ src/nl/b3p/commons/services/hibernate/SelectHelper.java
@@ -4,7 +4,6 @@
  * Created on 30 juni 2005, 10:00
  *
  */
-
 package nl.b3p.commons.services.hibernate;
 
 import java.lang.reflect.InvocationTargetException;
@@ -14,6 +13,8 @@ import java.util.Collection;
 import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.apache.commons.beanutils.BeanUtilsBean;
 import org.apache.commons.beanutils.ConversionException;
 import org.apache.commons.beanutils.ConvertUtilsBean;
@@ -30,21 +31,21 @@ import org.hibernate.Session;
  * @author Chris
  */
 public class SelectHelper {
-    
+
     protected static Log log = LogFactory.getLog("nl.b3p.commons.services.SelectHelper");
-    
     protected Session sess = null;
-    
-    public SelectHelper() {}
-    
+
+    public SelectHelper() {
+    }
+
     public Query queryBuilder(Map searchList, boolean andNotOr) throws HibernateException {
         return null;
     }
-    
+
     public Object addToReport(Object report, ArrayList recordsList) {
         return report;
     }
-    
+
     public static StringBuffer buildWhere(ArrayList whereSnippets, boolean andNotOr) {
         StringBuffer whereClause = new StringBuffer();
         Iterator it = whereSnippets.iterator();
@@ -55,65 +56,80 @@ public class SelectHelper {
             if (first) {
                 whereClause.append(" where ");
             } else {
-                if (andNotOr)
+                if (andNotOr) {
                     whereClause.append(" and ");
-                else
+                } else {
                     whereClause.append(" or ");
+                }
             }
             whereClause.append("(").append(snippet).append(")");
             first = false;
         }
         return whereClause;
     }
-    
+
     public static Query addParams(Query q, Hashtable hqlParams) throws HibernateException {
-        if (q==null || hqlParams==null || hqlParams.isEmpty())
+        if (q == null || hqlParams == null || hqlParams.isEmpty()) {
             return q;
         // Maak de query
+        }
         Iterator it = hqlParams.keySet().iterator();
         String param = null;
         while (it.hasNext()) {
             param = (String) it.next();
             Object pv = hqlParams.get(param);
-            if (pv instanceof Object[])
-                q.setParameterList(param, (Object[])pv);
-            else if (pv instanceof Collection)
-                q.setParameterList(param, (Collection)pv);
-            else
+            if (pv instanceof Object[]) {
+                q.setParameterList(param, (Object[]) pv);
+            } else if (pv instanceof Collection) {
+                q.setParameterList(param, (Collection) pv);
+            } else {
                 q.setParameter(param, pv);
+            }
         }
         return q;
     }
-    
+
     public static boolean copyHtoX(Object xo, Object ho) {
-        if (ho==null || xo==null)
+        if (ho == null || xo == null) {
             return false;
-        
+        }
+
         PropertyUtilsBean pub = new PropertyUtilsBean();
         // Bij probleem met conversie return null en geen exception gooien
         Converter bdConverter = new org.apache.commons.beanutils.converters.BigDecimalConverter(null);
         ConvertUtilsBean cub = new ConvertUtilsBean();
         cub.register(bdConverter, BigDecimal.class);
         BeanUtilsBean bub = new BeanUtilsBean(cub, pub);
-        
+
+        Map hoMap = null;
         try {
-            bub.copyProperties(xo, ho);
-            /* FIXME indien exception optreedt zijn waarschijnlijk nog niet alle 
-             * properties gekopieerd! 
-             * Zijn exceptions niet gewoon echt fouten??
-             */
-        } catch (IllegalAccessException iae){
-            log.debug("IllegalAccessException: " +  iae.getMessage());
-        } catch (IllegalArgumentException  ige){
-            log.debug("IllegalArgumentException : " +  ige.getMessage());
-        } catch (InvocationTargetException ite){
-            log.debug("InvocationTargetException: " +  ite.getMessage());
-        } catch (ClassCastException cce){
-            log.debug("ClassCastException: " +  cce.getMessage());
-        } catch (ConversionException ce){
-            log.debug("ConversionException: " +  ce.getMessage());
+            hoMap = bub.describe(ho);
+        } catch (Exception ex) {
+            log.error("", ex);
+        }
+        if (hoMap == null || hoMap.isEmpty()) {
+            return false;
+        }
+        Iterator it = hoMap.keySet().iterator();
+        while (it.hasNext()) {
+            String name = (String) it.next();
+            try {
+                Object val = bub.getSimpleProperty(ho, name);
+                bub.setProperty(xo, name, val);
+            } catch (NoSuchMethodException nsme) {
+                log.debug("NoSuchMethodException: " + nsme.getMessage());
+            } catch (IllegalAccessException iae) {
+                log.debug("IllegalAccessException: " + iae.getMessage());
+            } catch (IllegalArgumentException ige) {
+                log.debug("IllegalArgumentException : " + ige.getMessage());
+            } catch (InvocationTargetException ite) {
+                log.debug("InvocationTargetException: " + ite.getMessage());
+            } catch (ClassCastException cce) {
+                log.debug("ClassCastException: " + cce.getMessage());
+            } catch (ConversionException ce) {
+                log.debug("ConversionException: " + ce.getMessage());
+            }
         }
         return true;
     }
-    
-}
+ }
